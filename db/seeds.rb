2bbf66d900dcc4081306420a951b0db4e4a8990f
Arian Khosravi <arian.khosravi@gmail.com>
2017-08-04 16:26:28 -0700

    use badge id instead of id, let ids be autogenerated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Vendor.delete_all
 csv_text = File.read('db/Street-Sense-Master-Vendor-Roster.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
-  vendor = Vendor.find_or_create_by(id: row['id'])
+  vendor = Vendor.find_or_create_by(badge_id: row['id'])
   vendor.name = "#{row['first_name']} #{row['last_name']}"
   vendor.in_app = false
   vendor.has_back_issues = false
@@ -17,7 +17,7 @@ end
 csv_text = File.read('db/Street-Sense-Active-Vendors.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
-  vendor = Vendor.find_or_create_by(id: row['id'])
+  vendor = Vendor.find_or_create_by(badge_id: row['id'])
   vendor.name = row['full_name']
   vendor.in_app = true
   vendor.has_back_issues = false
